095eabea2aff547c008f6ac0de9ca490ae568d07
Mitchell Gates <md.fragala@gmail.com>
2015-08-07 13:30:56 -0400

    Close infoWindows on changing category.

--- main.js
+++ main.js
@@ -1807,15 +1807,17 @@ var LOCATIONS = [{
    $('.helper').click(function() {
       var type = $(this).attr('class').match(/mt-(\w+)\s?/)[1];
       MAP.searchMarkers(function(m) {
-         return m.category !== type;
+         return m.category !== 'master' && m.category !== type;
       }, function() {
          this.setVisible(false);
+         if (this.infoWindow) this.infoWindow.close();
       });
       MAP.searchMarkers(function(m) {
          var proof = (type === 'showall') ? m.category : type;
          return m.category === 'master' || m.category === proof;
       }, function() {
          this.setVisible(true);
+         if (this.infoWindow) this.infoWindow.close();
       });
    });
 
